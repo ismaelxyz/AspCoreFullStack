c1aaa5b7374cd43394b643bd149a89b5e7ace80c
0CrazyLove <axelvasquez582@gmail.com>
2025-09-07 23:29:32 -0500

    ...

---
+++ aspnetcore-crud-put-validations/Controllers/UserApiController.cs
@@ -0,0 +1,31 @@
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("Api/[controller]")]
+public class UserApiController : ControllerBase
+{
+    private readonly UserDbContext _context;
+    public UserApiController(UserDbContext context)
+    {
+        _context = context;
+    }
+    [HttpGet]
+    public IActionResult GetUsers()
+    {
+        var user = _context.Users;
+        return Ok(user);
+    }
+    [HttpPost]
+    public IActionResult CreateUsers(User user)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetUsers), user);
+        }
+        return BadRequest(ModelState);
+    }
+
+}
---
+++ aspnetcore-crud-put-validations/Program.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews(); // Habilita controladores MVC con soporte para vistas Razor.
+builder.Services.AddDbContext<UserDbContext>(Options =>
+Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseStaticFiles(); // Habilita archivos estáticos (css, js, imágenes) desde wwwroot.
+app.MapControllers(); // Habilita el mapeo de rutas por atributos en los controladores.
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+app.Run();
+
