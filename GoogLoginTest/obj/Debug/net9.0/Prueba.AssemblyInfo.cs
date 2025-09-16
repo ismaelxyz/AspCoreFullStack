fae1c579f6435b96ae1eae10dae00b7fc3e0aab7
0CrazyLove <axelvasquez582@gmail.com>
2025-09-15 21:01:51 -0500

    ...

--- GoogLoginTest/obj/Debug/net9.0/Prueba.AssemblyInfo.cs
+++ GoogLoginTest/obj/Debug/net9.0/Prueba.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7caa1ca68ce4d2f115cb4cce55adc2e768c5222f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18819af319f2ae5303649c5e450ad749f515807b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
